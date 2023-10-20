cf4a05efe7068909f498aa084904f4cafebf7f85
KustelR <dmitriysstudios2@gmail.com>
2023-10-21 00:57:13 +0400

    Passing additional data to article database

--- src/requestHandlers/postArticle.js
+++ src/requestHandlers/postArticle.js
@@ -1,5 +1,26 @@
+import { verifyToken } from "../auth/jwt_gen.js";
+
+
 async function handle(req, res, mongoAPI) {
-    const article = req.body;
+    const rawArticle = req.body;
+    let article = rawArticle;
+    console.log(rawArticle)
+    if (!rawArticle.accessToken) {
+        res.status(403).json({messageTitle: "Failure", message: "You need to be authorized to perfrom this action"});
+        return;
+    }
+    try {
+        const authToken = await verifyToken(rawArticle.accessToken);
+        article.authorLogin = authToken.data.login;
+    }
+    catch (error) {
+        if (error.message == "jwt expired") {
+            res.status(403).json({messageTitle: "Failure", message: "You need to be authorized to perfrom this action"});
+            return;
+        }
+    }
+    article.tags = rawArticle.tags.split(', ');
+    article.postTime = new Date();
     mongoAPI.connect('myreactapp');
     try {
         await mongoAPI.createArticle(article)
@@ -8,7 +29,8 @@ async function handle(req, res, mongoAPI) {
         res.status(200).json({messageTitle: "FAILURE!", message: err.message})
         return
     }
-    res.status(200).json({messageTitle: "Glorious success!", message: "Post was mischefiously created!"})
+    res.status(200).json({messageTitle: "Glorious success!", message: "Post was created successfully!"})
 }
+
 
 export default handle
