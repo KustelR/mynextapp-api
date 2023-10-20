53af5b357f9a6151351ba8ff20210b609e5a8f15
KustelR <dmitriysstudios2@gmail.com>
2023-10-21 01:44:04 +0400

    Now checks if fields are empty+(kinda) xss-proofed

--- src/requestHandlers/postArticle.js
+++ src/requestHandlers/postArticle.js
@@ -4,12 +4,13 @@ import { verifyToken } from "../auth/jwt_gen.js";
 async function handle(req, res, mongoAPI) {
     const rawArticle = req.body;
     let article = rawArticle;
-    console.log(rawArticle)
-    if (!rawArticle.accessToken) {
-        res.status(403).json({messageTitle: "Failure", message: "You need to be authorized to perfrom this action"});
-        return;
-    }
+    
     try {
+        if (!rawArticle.accessToken) {
+            res.status(403).json({messageTitle: "Failure", message: "You need to be authorized to perfrom this action"});
+            return;
+        }
+
         const authToken = await verifyToken(rawArticle.accessToken);
         article.authorLogin = authToken.data.login;
     }
@@ -19,7 +20,19 @@ async function handle(req, res, mongoAPI) {
             return;
         }
     }
+
+    if (!(rawArticle.body && rawArticle.tags && rawArticle.description)) {
+        res.status(400).json({messageTitle: "Failure", message: "Not all fields provided"});
+            return;
+    }
+
+    if (rawArticle.body.includes('<script>')) {
+        res.status(400).json({messageTitle: "Failure", message: "Found blacklisted tags"});
+        return;
+    }
+
     article.tags = rawArticle.tags.split(', ');
+
     article.postTime = new Date();
     mongoAPI.connect('myreactapp');
     try {
