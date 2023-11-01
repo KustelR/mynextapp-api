bad07c8d5567b4369e63b2902f554173977c72f7
KustelR <dmitriysstudios2@gmail.com>
2023-11-01 21:39:06 +0400

    getPreviews adapted for new API ver

--- src/requestHandlers/articles/getPreviews.js
+++ src/requestHandlers/articles/getPreviews.js
@@ -1,18 +1,9 @@
 import getArticlesPreviews from "../../database/methods/articles/getPreviews.js";
 
 
-async function ArticlePreviews(req, res) {
-    const articles = await getArticlesPreviews(req.query)
-
-    if (articles) {
-        res.status(200).json(articles);
-        for (let i = 0; i < articles.length; i++) {
-            articles[i].body = "";
-        }
-    }
-    else {
-        res.status(404).json({messageTitle: "FAILURE", message: "No articles was found"})
-    }
+async function ArticlePreviews(query) {
+    const articles = await getArticlesPreviews(query);
+    return articles;
 }
 
 export default ArticlePreviews
