930a7703bb3ab222720f68738601857aa96f09eb
KustelR <dmitriysstudios2@gmail.com>
2023-11-01 21:38:06 +0400

    authUser rewritten in TS

--- src/utils/authUser.ts
+++ src/utils/authUser.ts
@@ -1,13 +1,18 @@
-import { verifyToken } from "../../../auth/jwt_gen.js";
+import { verifyToken } from "../auth/jwt_gen.js";
+
+
+type Result = {
+    [key: string]: any
+}
 
 
 /**
  * Checks auth token and returns values of requested fields
- * @param {object} token JWT token
+ * @param {string} token JWT token
  * @param {[string]} validate Array of permission names to validate
  * @returns {object} Map of permissions and userdata requested in ```validate```
  */
-export default async function auth(token, validate) {
+export default async function authUser(token: any, validate: Array<string>): Promise<Result | null> {
     let authToken;
 
     try {
@@ -16,8 +21,8 @@ export default async function auth(token, validate) {
     catch (err) {
         return null;
     }
-
-    let result = {};
+    let result: Result;
+    result = {};
     for (let i = 0; i < validate.length; i++) {
         result[validate[i]] = authToken.data[validate[i]];
     }
