b7c3245b648fe1a16497ab93c55b3c3bf4ef06c0
KustelR <dmitriysstudios2@gmail.com>
2023-11-08 18:40:28 +0400

    Moved update to TS

---
+++ src/requestHandlers/articles/update.ts
@@ -0,0 +1,39 @@
+import updateArticle from "../../database/methods/articles/update.js";
+
+interface keyable {
+  [key: string]: any;
+}
+
+/**
+ * updates article based on given data
+ * @param data Article data to be updated
+ * @param query url query string from request
+ * @param authorLogin author login extracted from JWT
+ * @param dbCall Function from db driver
+ * @returns
+ */
+export default async function handle(
+  data: keyable,
+  query: any,
+  headers: any,
+  authorLogin: string | null,
+  dbCall = updateArticle
+): Promise<keyable | null> {
+  let result: keyable;
+
+  result = await dbCall(query, data);
+  if (result.authorLogin !== authorLogin) {
+    throw new Error()
+  }
+  if (headers["accept"]) {
+    const requestedContentType = headers["accept"].split(";");
+    if (
+      requestedContentType[0] === "application/json" &&
+      requestedContentType[1] === "article"
+    ) {
+      return result;
+    }
+  }
+
+  return null;
+}
